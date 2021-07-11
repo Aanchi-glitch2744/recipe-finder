7df0c30aad777f448e2046b5ef8aa4fdc34360c5
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2021-07-11 13:25:27 +0530

    Added Searchbar in Header with Styling

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./common/header.css";
 
-const Header = function() {
+class HeaderForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searched: false,
+      foodItem: "",
+    };
+  }
+
+  getRecipe = (e) => {
+    e.preventDefault();
+
+    var item = document.getElementById("text").value;
+
+    this.setState({
+      searched: true,
+      foodItem: item,
+    });
+  };
+
+  render() {
     return (
-        <div className="header">
-            Recipe Finder
+      <div>
+        <div className="header-container">
+          <h1 className="heading">Recipe Finder</h1>
+          <form className="input-form">
+            <input
+              type="text"
+              placeholder="Enter the Name of the Dish"
+              id="text"
+            />
+            <input
+              type="submit"
+              value="Get Ingredients"
+              onClick={this.getRecipe}
+            />
+          </form>
         </div>
-    )
+    
+      </div>
+    );
+  }
 }
-
-export default Header;
+export default HeaderForm;
